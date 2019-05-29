8ce34ba777b2b6e0db89fc808f4a6aa62e6dc960
Krzysztof <krzysztof.otrebski@gmail.com>
2019-05-29 23:05:39 +0200

    Js cleaning

--- gui/src/components/HistoryStep.js
+++ gui/src/components/HistoryStep.js
@@ -6,17 +6,14 @@ import { Link } from "react-router-dom";
 class HistoryStep extends React.Component {
 
     render() {
-        let finalStep = <div />
+        let finalStep = <div />;
         if (this.props.success === true) {
             finalStep = <div>Great success</div>
         } else if (this.props.success === false) {
             finalStep = <div>Failure!</div>
         }
         const answers = this.props.answers.map(a => {
-            let variant="light"
-            if (a.selected){
-                variant="success"
-            }
+            let variant= a.selected ? "light" : "success";
 
             return <ListGroup.Item
                 key={a.id}
@@ -27,7 +24,7 @@ class HistoryStep extends React.Component {
             <Link to={`/quizz/${this.props.quizzId}/path/${a.id}`}>{a.text}</Link>
             </ListGroup.Item>
         }
-        )
+        );
 
         return (
             <div className="historyStep">

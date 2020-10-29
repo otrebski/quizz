6094bc5c1d1de2bb8f44c64bdc4f445c659bbfbf
Krzysztof <krzysztof.otrebski@gmail.com>
2020-10-30 00:38:04 +0100

    Selenium tests

---
+++ src/selenium/java/gui/QuizzTest.java
@@ -0,0 +1,39 @@
+package gui;
+
+import gui.pages.Home;
+import org.fluentlenium.adapter.junit.FluentTest;
+import org.fluentlenium.core.annotation.Page;
+import org.junit.Test;
+
+//@FluentConfiguration(capabilities = "{\"chromeOptions\": {\"args\": [\"headless\",\"disable-gpu\"]}}")
+public class QuizzTest extends FluentTest {
+
+    @Page
+    Home homePage;
+
+    @Test
+    public void goThroughQuizz() throws InterruptedException {
+        //TODO upload quizz
+        // * Question 1
+        //  - Answer 1.1
+        //   * Question 1.1.1
+        //   * Question 1.1.2
+        //  - Answer 1.2
+        //   * Question 1.2.1
+        //    - Answer 1.2.1 1
+        //    - Answer 1.2.1 2
+        //   * Question 1.2.2
+        //
+        
+        goTo(homePage)
+                .selectQuizz("simple_tree.json")
+                .select("Right")
+                .select("Right")
+                .validateIsFinal("Right Right Node");
+    }
+
+    @Override
+    public String getWebDriver() {
+        return "chrome";
+    }
+}

df891d9821166c04acdc034c0bb9d90a165ddf2f
Krzysztof <krzysztof.otrebski@gmail.com>
2020-11-13 18:11:31 +0100

    Improve UI and error handling

--- gui/src/components/Quizzes.js
+++ gui/src/components/Quizzes.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 import {Link} from "react-router-dom";
 import Alert from 'react-bootstrap/Alert'
+import Button from 'react-bootstrap/Button'
+
+import "./Quizzes.css"
 
 class Quizzes extends React.Component {
 
@@ -13,30 +16,43 @@ class Quizzes extends React.Component {
             loading: true,
             error: ""
         }
-        this.loadQuizzes()
+        this.loadQuizzes(true)
     }
 
-    loadQuizzes = () => {
-        this.setState({
-            loading: true,
-            loadingError: false,
-            error: ""
-        })
+    loadQuizzes = (initial) => {
+        if (!initial) {
+            this.setState({
+                loading: true,
+                loadingError: false,
+                error: ""
+            })
+        }
         this.props.loadAction()
             .then(q =>
                 this.setState({
                     quizzes: q.quizzes,
-                    errorQuizzes: q.errorQuizzes
+                    errorQuizzes: q.errorQuizzes,
+                    loading: false
                 })
             ).catch(e => {
-            this.setState({loading: false, loadingError: true, error: e.message, quizzes:[], errorQuizzes:[]})
-        })
+                this.setState({loading: false, loadingError: true, error: e.message, quizzes: [], errorQuizzes: []})
+            }
+        )
     };
 
     render() {
-        const noQuizz = (this.state.quizzes.length === 0) ? <div>No quizzes</div> : <div/>
+        const noQuizz = (this.state.quizzes.length === 0) ? <div><Alert variant={"secondary"}>
+            <Alert.Heading>There is no quizzes defined</Alert.Heading>
+            <hr/>
+            <div>
+               Check in documentation how to define and load quizz.
+            </div>
+
+        </Alert></div> : <div/>
         const quizzes = this.state.quizzes.map(q =>
-            <div id="quizz-link" key={q.id}><Link id={q.id} to={`/quizz/${q.id}`}>{q.title}</Link></div>);
+            <div id="quizz-link" key={q.id} className="quizzLink">
+                <Link id={q.id} to={`/quizz/${q.id}`}>{q.title}</Link>
+            </div>);
         const error = this.state.loadingError ? <Alert variant={"danger"}>Can't load data: {this.state.error}</Alert> : <div/>
         const invalidQuizzes = (this.state.errorQuizzes.length !== 0) ? <div><Alert variant={"warning"}>
             <Alert.Heading>Following quizzes can't be parsed {this.state.errorQuizzes.size}: </Alert.Heading>
@@ -54,6 +70,8 @@ class Quizzes extends React.Component {
                 {error}
                 {quizzes}
                 {invalidQuizzes}
+                <Button variant="light" disabled={this.state.isLoading}
+                        onClick={!this.state.loading ? () => this.loadQuizzes(false) : null}>{this.state.loading ? 'Loading…' : 'Click to reload'}</Button>
             </div>
         );
     }
--- gui/src/stories/index.stories.js
+++ gui/src/stories/index.stories.js
@@ -2,12 +2,10 @@ import React from 'react';
 
 import {storiesOf} from '@storybook/react';
 import {MemoryRouter} from "react-router-dom"; // our router
-import {linkTo} from '@storybook/addon-links';
 import Step from '../components/Step'
 import HistoryStep from '../components/HistoryStep'
 import Quizz from '../components/Quizz'
 
-import Welcome from '@storybook/react/demo';
 import Feedback from '../components/Feedback';
 import 'bootstrap/dist/css/bootstrap.css';
 import Quizzes from "../components/Quizzes";
@@ -23,7 +21,28 @@ const historyAnswers = [
     {id: "id3", text: "Maybe"}
 ]
 
-const quizzState = JSON.parse(`{
+const quizzStateStarting = JSON.parse(`{
+  "path": "",
+  "currentStep": {
+    "id": "buildingFuses",
+    "question": "Are fuses outside your apartment ok?",
+    "answers": [
+      {
+        "id": "noPower",
+        "text": "yes",
+        "selected": null
+      },
+      {
+        "id": "buildingFusesBroken",
+        "text": "no",
+        "selected": null
+      }
+    ],
+    "success": null
+  },
+  "history": []
+}`)
+const quizzStateInProgress = JSON.parse(`{
     "path": "electricity;checkLocal;buildingFuses",
     "currentStep": {
       "id": "buildingFuses",
@@ -188,23 +207,42 @@ const quizzesWithErrors = JSON.parse(`{
 const multiline = `
 This is a **question** with *markdown*.
 Can you see [image](https://image.flaticon.com/icons/png/128/2938/2938229.png) with cat ![Cat](https://image.flaticon.com/icons/png/128/2938/2938229.png)?`
-storiesOf('Welcome!', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')}/>);
+// storiesOf('Welcome', module).add('to Storybook', () => <div>Hello</div>);
 
-storiesOf('Components', module)
+storiesOf('Main page', module)
     .addDecorator(story => (
         <MemoryRouter initialEntries={['/']}>{story()}</MemoryRouter>
     ))
+    .add("Quizzes loading error", () => <Quizzes loadAction={() => new Promise((res, rej) => rej(new Error("server error")))}/>)
+    .add("Quizzes loaded with errors", () => <Quizzes loadAction={() => new Promise((res) => res(quizzesWithErrors))}/>)
+    .add("Quizzes all loaded", () => <Quizzes loadAction={() => new Promise((res) => res(quizzes))}/>)
+;
 
+storiesOf("Step", module)
+    .addDecorator(story => (
+        <MemoryRouter initialEntries={['/']}>{story()}</MemoryRouter>
+    ))
     .add("Step", () => <Step question="How do you feel today?" action={(a) => console.log("Selected ", a)} answers={answers}/>)
     .add("Step with markdown", () => <Step question={multiline} action={(a) => console.log("Selected ", a)} answers={answers}/>)
     .add("Step success", () => <Step question="How do you feel today?" action={(a) => console.log("Selected ", a)} success={true} answers={[]}/>)
     .add("Step failure", () => <Step question="How do you feel today?" action={(a) => console.log("Selected ", a)} success={false} answers={[]}/>)
     .add("HistoryStep", () => <HistoryStep question="How do you feel today?" answers={historyAnswers} path={["a", "b"]}/>)
     .add("HistoryStep with markdown", () => <HistoryStep question="How do you *feel* **today?**" answers={historyAnswers} path={["a", "b"]}/>)
+
+storiesOf("Quizz", module)
+    .addDecorator(story => (
+        <MemoryRouter initialEntries={['/']}>{story()}</MemoryRouter>
+    ))
+    .add("Quizz starting", () => <Quizz quizzId="q1" path={[""]} selectAction={(q, s) => new Promise((res) => res(quizzStateStarting))}/>)
+    .add("Quizz in progress", () => <Quizz quizzId="q1" path={["electricity;checkLocal"]} selectAction={(q, s) => new Promise((res) => res(quizzStateInProgress))}/>)
+    .add("Quizz finished", () => <Quizz quizzId="q1" quizzState={quizzStateFinish} selectAction={(q, s) => new Promise((res) => res(quizzStateFinish))}/>)
+
+
+storiesOf('Other', module)
+    .addDecorator(story => (
+        <MemoryRouter initialEntries={['/']}>{story()}</MemoryRouter>
+    ))
     .add("Feedback", () => <Feedback path="a;b;c"/>)
-    .add("Quizz", () => <Quizz quizzId="q1" path={["electricity;checkLocal"]} selectAction={(q, s) => new Promise((res, rej) => res(quizzState))}/>)
-    .add("Quizz finished", () => <Quizz quizzId="q1" quizzState={quizzStateFinish} selectAction={(q, s) => new Promise((res, rej) => res(quizzStateFinish))}/>)
-    .add("Quizzes loading error", () => <Quizzes loadAction={() => new Promise((res, rej) => rej(new Error("server error")))}/>)
-    .add("Quizzes loaded with errors", () => <Quizzes loadAction={() => new Promise((res, rej) => res(quizzesWithErrors))}/>)
-    .add("Quizzes all loaded", () => <Quizzes loadAction={() => new Promise((res, rej) => res(quizzes))}/>)
 ;
+
+
